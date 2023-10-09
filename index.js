1c5839fafbbd98d0f32c3d8d279b9b4f7ef695cc
parisosuch-dev <parisosuch.dev@gmail.com>
2023-10-08 21:12:03 -0700

    init program

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+#!usr/bin/env node
+
+const { Command } = require("commander");
 const figlet = require("figlet");
 
-console.log(figlet.textSync("cook 👨‍🍳"));
+program = new Command();
+
+program
+  .name("cook")
+  .version("1.0.0")
+  .description(
+    "A todo list CLI -- nothing to do with actually cooking. It's all a metaphor."
+  );
+
+program.parse();
